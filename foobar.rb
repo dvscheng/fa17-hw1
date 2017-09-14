b86d7d391b7d7f14218643b36f06db03d1ad2804
David Cheng <dvscheng@berkeley.edu>
2017-09-13 19:15:06 -0700

    finished hw01

--- foobar.rb
+++ foobar.rb
@@ -1,8 +1,14 @@
 class Foobar
 
   def self.baz(a)
-    # Class method
-    # Call with `Foobar.baz`
+    a.map! { |x| x.to_i }
+    a.map! { |x| x + 2 }
+    a.keep_if { |x| x % 2 == 0 }
+    a.uniq!
+    a.keep_if { |x| x <= 10 }
+    sum = 0
+    a.each { |x| sum += x }
+    sum
   end
 end
 
